3588fced9b2ccba91850dab3deabe62fe00b18db
Sam Gammon <s@mg.sv>
2023-12-17 03:10:31 +0000

    chore: update deps, plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,15 @@
-import build.less.plugin.settings.buildless
-
 // ...
 pluginManagement {
     repositories {
+        maven("https://gradle.pkg.st")
+        maven("https://maven.pkg.st")
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 plugins {
-    id("build.less") version "1.0.0-beta6"
+    id("build.less") version "1.0.0-rc2"
 }
 
 // ...
@@ -18,6 +18,7 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        pkgst()
         google()
         mavenCentral()
     }
@@ -27,5 +28,5 @@ rootProject.name = "sample-android-gradle"
 include(":app")
 
 buildless {
-    // settings go here
+    // settings go here (this block is optional)
 }
